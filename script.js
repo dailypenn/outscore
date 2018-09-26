04f39058b7b37a2e0374f96550541b5636a440f6
Sneha Rampalli <svrampalli@gmail.com>
2018-09-26 18:09:47 -0400

    Added team names/scores
    Cleaned up code to include what I need and added inputs for team names and scores.

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 var name = "Amy Gutmann";
-var opposingTeamScore = 0;
-var pennScore = 0;
-var opposingTeam = "Insert Team";
 var title = "Penn President";
-var showAttribution = false;
 var includeLogo = true;
 var inverseColors = false;
 var useWordmark = false;
-var useSports = true;
+var useSports = false;
 var includePhoto = false;
 var photoURL = "";
 var centerElements = true;
+var teamOneName = "Team One";
+var teamTwoName = "Team Two";
+var teamOneScore = 0;
+var teamTwoScore = 0;
 
 // colors by publication
-const primary = ['#aa1e22', '#44bfbf', '#446cb3'];
+const primary = ['#aa1e22', '#44bfbf', '#456CB3'];
 const logo = ['logos/dp.svg', 'logos/street.svg', 'logos/utb.svg'];
-const inverse = ['logos/inverse-dp.svg', 'logos/inverse-street.svg', 'logos/utb.svg'];
+const inverse = ['logos/inverse-dp.svg', 'logos/inverse-street.svg', 'logos/inverse-utb.svg'];
 
 var wrapText = function(context, text, x, y, maxWidth, lineHeight) {
   var words = text.split(' ');
@@ -45,28 +45,27 @@ var renderContent = function() {
   var pub = document.getElementById("publicationSelect").selectedIndex;
 
   // QUOTE TEXT + BG + IMG
-  var quoteCtx = canvas.getContext("2d");
-  quoteCtx.fillStyle = primary[pub];
-  quoteCtx.fillRect(0, 0, canvas.width, canvas.height);
+  var gameContext = canvas.getContext("2d");
+  gameContext.fillStyle = primary[pub];
+  gameContext.fillRect(0, 0, canvas.width, canvas.height);
   if (inverseColors) {
-    quoteCtx.fillStyle = "#ffffff";
-    quoteCtx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
+    gameContext.fillStyle = "#ffffff";
+    gameContext.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
   }
 
-  quoteCtx.font = "200 38px lora";
+  gameContext.font = "200 38px Oswald";
   if (inverseColors) {
-    quoteCtx.fillStyle = primary[pub];
+    gameContext.fillStyle = primary[pub];
   } else {
-    quoteCtx.fillStyle = "#ffffff";
+    gameContext.fillStyle = "#ffffff";
   }
+
+  gameContext.textAlign = "center";
 
   /* * * * * * * * * * * * * * * * * * * * *
   * RENDER CANVAS WITH PHOTO
   * * * * * * * * * * * * * * *  * * * * * */
   if (includePhoto && photoURL != "") {
-    var attribY = wrapText(quoteCtx, "\“Filler\”", 520,
-      canvas.height / 2 - (showAttribution ? 100 : 70), 460, 48);
-
     // load logo
     if (includeLogo) {
       var image = new Image();
@@ -74,10 +73,10 @@ var renderContent = function() {
         var aspect = image.width / image.height;
         if (useWordmark) {
           var width = 40 * aspect;
-          quoteCtx.drawImage(image, canvas.width - width - 40, 40, width, 40);
+          gameContext.drawImage(image, canvas.width - width - 40, 40, width, 40);
         } else {
           var width = 50 * aspect;
-          quoteCtx.drawImage(image, canvas.width - width - 40, 40, width, 50);
+          gameContext.drawImage(image, canvas.width - width - 40, 40, width, 50);
         }
       }
     }
@@ -85,37 +84,20 @@ var renderContent = function() {
     // load photo
     var photo = new Image();
     photo.onload = function() {
-      quoteCtx.drawImage(photo, 30, 30, 440, 440);
+      gameContext.drawImage(photo, 30, 30, 440, 440);
     }
     photo.src = photoURL;
-
-    if (showAttribution) {
-      quoteCtx.textAlign = "left"; // makes below calculations work\
-      var nameCtx = canvas.getContext("2d");
-      var titleCtx = canvas.getContext("2d");
-
-      // NAME TEXT
-      nameCtx.font = "38px neuzeit-grotesk";
-      if (inverseColors) {
-        nameCtx.fillStyle = primary[pub];
-      } else {
-        nameCtx.fillStyle = "#ffffff";
-      }
-      nameCtx.fillText(name, 520, attribY + 100 < (canvas.height - 70) ? attribY + 100 : canvas.height - 70);
-
-      // TITLE TEXT
-      titleCtx.font = "100 30px neuzeit-grotesk";
-      titleCtx.fillText(title, 520, attribY + 140 < (canvas.height - 30) ? attribY + 140 : canvas.height - 30);
-  	}
-
 
   /* * * * * * * * * * * * * * * * * * * * *
   * RENDER CANVAS WITHOUT PHOTO
   * * * * * * * * * * * * * * *  * * * * * */
   } else {
-    var quoteBottomSpacing = (showAttribution ? 0 : -20) + (includeLogo ? 50 : 120)
-     + (!showAttribution && !includeLogo ? 40 : 0);
-    wrapText(quoteCtx, "\“Filler\”", 50,
+    var quoteBottomSpacing = -20 + (includeLogo ? 50 : 120)
+     + (!includeLogo ? 40 : 0);
+    wrapText(gameContext, teamOneName.toUpperCase(), centerElements ? 250 : 50,
+      canvas.height / 2 - quoteBottomSpacing, 800, 48);
+
+    wrapText(gameContext, teamTwoName.toUpperCase(), centerElements ? 750 : 50,
       canvas.height / 2 - quoteBottomSpacing, 800, 48);
 
     // load logo
@@ -124,37 +106,10 @@ var renderContent = function() {
       image.onload = function() {
         var aspect = image.width / image.height;
         var width = 50 * aspect;
-        var centerWidth = canvas.width / 2;
-        quoteCtx.drawImage(image, centerWidth - (width / 2), 50,
+        gameContext.drawImage(image, (centerElements ? (canvas.width - width) / 2 + 10 : canvas.width - width - 50), 50,
           width, 50);
       }
     }
-
-    if (showAttribution) {
-      quoteCtx.textAlign = "left"; // makes below calculations work
-      var nameCtx = canvas.getContext("2d");
-      var titleCtx = canvas.getContext("2d");
-      var nameLength = nameCtx.measureText(name + " |").width;
-      var titleLength = titleCtx.measureText(title).width;
-
-      var nameCtxX = 50;
-      var titleCtxX = nameLength + nameCtxX + 30;
-
-      // NAME TEXT
-      nameCtx.font = "38px neuzeit-grotesk";
-      if (inverseColors) {
-        nameCtx.fillStyle = primary[pub];
-      } else {
-        nameCtx.fillStyle = "#ffffff";
-      }
-      nameCtx.fillText(name + " |", nameCtxX, canvas.height - (includeLogo ? 70 : 120)
-        + (!includeLogo && showAttribution ? 50 : 0));
-
-      // TITLE TEXT
-      titleCtx.font = "100 38px neuzeit-grotesk";
-      titleCtx.fillText(title, titleCtxX, canvas.height - (includeLogo ? 70 : 120)
-        + (!includeLogo && showAttribution ? 50 : 0));
-  	}
   }
 
   // add logo
@@ -183,18 +138,23 @@ window.setTimeout(function() {
   renderContent();
 }, 700);
 
-document.getElementById('pennScore').oninput = function() {
-  pennScore = this.value;
+document.getElementById('teamOneName').oninput = function() {
+  teamOneName = this.value;
   renderContent();
 }
 
-document.getElementById('opposingTeamScore').oninput = function() {
-  opposingTeamScore = this.value;
+document.getElementById('teamTwoName').oninput = function() {
+  teamTwoName = this.value;
   renderContent();
 }
 
-document.getElementById('opposingTeam').oninput = function() {
-  opposingTeam = this.value;
+document.getElementById('teamOneScore').oninput = function() {
+  teamOneScore = this.value;
+  renderContent();
+}
+
+document.getElementById('teamTwoScore').oninput = function() {
+  teamTwoScore = this.value;
   renderContent();
 }
 
@@ -208,69 +168,42 @@ document.getElementById('saveButton').addEventListener('click', function() {
   var blob = new Blob([asArray.buffer], {
     type: "image/png"
   });
-  saveAs(blob, "quote.png");
+  var filename = teamOneName + teamTwoName + "GameCard.png";
+  saveAs(blob, filename);
 });
 
 // EVENT HANDLERS
 
-// Toggle Attribution
-// var toggleAttrCheckbox = document.getElementById('toggleAttribution');
-// toggleAttrCheckbox.addEventListener('click', function() {
-// 	showAttribution = !showAttribution;
-// 	renderContent();
-// });
-
-// // Toggle Center Elements
-// var toggleCenterCheckbox = document.getElementById('centerElements');
-// toggleCenterCheckbox.addEventListener('click', function() {
-// 	centerElements = !centerElements;
-// 	renderContent();
-// });
-
 // Toggle Inverse Colors
 var toggleColorsCheckbox = document.getElementById('inverseColors');
 toggleColorsCheckbox.addEventListener('click', function() {
-	inverseColors = !inverseColors;
-	renderContent();
+  inverseColors = !inverseColors;
+  renderContent();
 });
 
-// Change Selected Publication
 var publicationSelect = document.getElementById('publicationSelect');
 publicationSelect.addEventListener('change', function() {
-  // wordmark and sports are only available for DP
-  if (document.getElementById('publicationSelect').selectedIndex != 0) {
-    document.getElementById('useWordmark').disabled = true;
-    document.getElementById('useWordmark').checked = false;
-    useWordmark = false;
-    document.getElementById('useSports').disabled = true;
-    document.getElementById('useSports').checked = false;
-    useSports = false;
-  } else {
-    document.getElementById('useWordmark').disabled = false;
-    document.getElementById('useSports').disabled = false;
-  }
-	renderContent();
+  renderContent();
 });
-
 // Toggle Wordmark
 var useWordmarkCheckbox = document.getElementById('useWordmark');
 useWordmarkCheckbox.addEventListener('click', function() {
   useWordmark = !useWordmark;
-	renderContent();
+  renderContent();
 });
 
-// Toggle Wordmark
+// Toggle Sports Logo
 var useSportsCheckbox = document.getElementById('useSports');
 useSportsCheckbox.addEventListener('click', function() {
   useSports = !useSports;
-	renderContent();
+  renderContent();
 });
 
 // Include Photo
 var togglePictureCheckbox = document.getElementById('togglePicture');
 togglePictureCheckbox.addEventListener('click', function() {
   includePhoto = !includePhoto;
-	renderContent();
+  renderContent();
 });
 
 // Upload Picture
@@ -287,8 +220,8 @@ fileInput.addEventListener('change', function() {
 });
 
 // Include logo
-// var toggleLogoCheckbox = document.getElementById('toggleLogo');
-// toggleLogoCheckbox.addEventListener('click', function() {
-//   includeLogo = !includeLogo;
-//   renderContent();
-// });
+var toggleLogoCheckbox = document.getElementById('toggleLogo');
+toggleLogoCheckbox.addEventListener('click', function() {
+  includeLogo = !includeLogo;
+  renderContent();
+});
