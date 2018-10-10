36a46e109594802556a1600d6d4fc932ca583539
ayhanefe <efeayhan1999@gmail.com>
2018-10-10 19:10:55 -0400

    Implemented inputs for color and score, removed different publications

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var name = "Amy Gutmann";
-var title = "Penn President";
 var includeLogo = true;
 var inverseColors = false;
 var useWordmark = false;
@@ -11,14 +9,16 @@ var teamOneName = "Team One";
 var teamTwoName = "Team Two";
 var teamOneScore = 0;
 var teamTwoScore = 0;
+var teamOneColor = '#ffffff';
+var teamTwoColor = '#ffffff';
 
 // colors by publication
-const primary = ['#aa1e22', '#44bfbf', '#456CB3'];
-const logo = ['logos/dp.svg', 'logos/street.svg', 'logos/utb.svg'];
-const inverse = ['logos/inverse-dp.svg', 'logos/inverse-street.svg', 'logos/inverse-utb.svg'];
+const primary = '#aa1e22';
+const logo = 'logos/dp.svg';
+const inverse = 'logos/inverse-dp.svg';
 
 var drawLine = function(gameContext, width, height) {
-  gameContext.beginPath(); 
+  gameContext.beginPath();
   gameContext.moveTo(width / 2 + 10, 125);
   gameContext.lineTo(width / 2 + 10, height - 50);
   gameContext.strokeStyle = '#ffffff';
@@ -50,12 +50,9 @@ var renderContent = function() {
   canvas.width = 1000;
   canvas.height = 500;
 
-  var pub = document.getElementById("publicationSelect").selectedIndex;
-
   // QUOTE TEXT + BG + IMG
   var gameContext = canvas.getContext("2d");
-  gameContext.fillStyle = primary[pub];
-
+  gameContext.fillStyle = primary;
   gameContext.fillRect(0, 0, canvas.width, canvas.height);
   if (inverseColors) {
     gameContext.fillStyle = "#ffffff";
@@ -64,7 +61,7 @@ var renderContent = function() {
 
   gameContext.font = "200 38px Oswald";
   if (inverseColors) {
-    gameContext.fillStyle = primary[pub];
+    gameContext.fillStyle = primary;
   } else {
     gameContext.fillStyle = "#ffffff";
   }
@@ -99,9 +96,9 @@ var renderContent = function() {
     // load photo
     var photo = new Image();
     photo.onload = function() {
-      gameContext.drawImage(photo, canvas.width / 4 - (sizeOfPic / 2), 
+      gameContext.drawImage(photo, canvas.width / 4 - (sizeOfPic / 2),
         canvas.height / 2 - (sizeOfPic / 2), sizeOfPic, sizeOfPic);
-      gameContext.drawImage(photo, (3 * canvas.width) / 4 - (sizeOfPic / 2), 
+      gameContext.drawImage(photo, (3 * canvas.width) / 4 - (sizeOfPic / 2),
         canvas.height / 2 - (sizeOfPic / 2), sizeOfPic, sizeOfPic);
     }
     photo.src = photoURL;
@@ -113,11 +110,23 @@ var renderContent = function() {
     drawLine(gameContext, canvas.width, canvas.height);
     var quoteBottomSpacing = -20 + (includeLogo ? 50 : 120)
      + (!includeLogo ? 40 : 0);
+
+    gameContext.fillStyle = teamOneColor;
     wrapText(gameContext, teamOneName.toUpperCase(), centerElements ? 250 : 50,
       canvas.height / 2 - quoteBottomSpacing, 800, 48);
 
+    gameContext.fillStyle = teamTwoColor;
     wrapText(gameContext, teamTwoName.toUpperCase(), centerElements ? 750 : 50,
       canvas.height / 2 - quoteBottomSpacing, 800, 48);
+
+    gameContext.fillStyle = teamOneColor;
+    gameContext.font = '200 144px Oswald';
+    gameContext.fillText(teamOneScore, centerElements ? 250 : 50,
+      canvas.height / 1.3);
+
+    gameContext.fillStyle = teamTwoColor;
+    gameContext.fillText(teamTwoScore, centerElements ? 750 : 50,
+      canvas.height / 1.3);
 
     // load logo
     if (includeLogo) {
@@ -146,9 +155,9 @@ var renderContent = function() {
     }
   } else {
     if (inverseColors) {
-      image.src = inverse[pub];
+      image.src = inverse;
     } else {
-      image.src = logo[pub];
+      image.src = logo;
     }
   }
 }
@@ -177,6 +186,20 @@ document.getElementById('teamTwoScore').oninput = function() {
   renderContent();
 }
 
+document.getElementById('teamOneColor').oninput = function() {
+  if (/^[0-9a-f]{3,6}$/.test(this.value)) {
+    teamOneColor = '#' + this.value;
+  }
+  renderContent();
+}
+
+document.getElementById('teamTwoColor').oninput = function() {
+  if (/^[0-9a-f]{3,6}$/.test(this.value)) {
+    teamTwoColor = '#' + this.value;
+  }
+  renderContent();
+}
+
 document.getElementById('saveButton').addEventListener('click', function() {
   var dataURL = canvas.toDataURL("image/png");
   var data = atob(dataURL.substring("data:image/png;base64,".length)),
@@ -200,10 +223,6 @@ toggleColorsCheckbox.addEventListener('click', function() {
   renderContent();
 });
 
-var publicationSelect = document.getElementById('publicationSelect');
-publicationSelect.addEventListener('change', function() {
-  renderContent();
-});
 // Toggle Wordmark
 var useWordmarkCheckbox = document.getElementById('useWordmark');
 useWordmarkCheckbox.addEventListener('click', function() {
